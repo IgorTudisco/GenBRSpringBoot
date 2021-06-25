da123b303580703f1a8f91c58baaf6ca5931103a
Igor Tudisco <igortudisco@hotmail.com>
2021-06-24 21:59:27 -0300

    Atividade - blog pessoal

---
+++ intermediario/BlogPessoal/src/main/java/org/blog/BlogPessoal/model/PostagensModel.java
@@ -0,0 +1,63 @@
+package org.blog.BlogPessoal.model;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table(name = "postagem")
+@Component
+public class PostagensModel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String titulo;
+	
+	@NotNull
+	@Size(min = 10, max = 500)
+	private String text;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date = new java.sql.Date(System.currentTimeMillis());
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+
+}
