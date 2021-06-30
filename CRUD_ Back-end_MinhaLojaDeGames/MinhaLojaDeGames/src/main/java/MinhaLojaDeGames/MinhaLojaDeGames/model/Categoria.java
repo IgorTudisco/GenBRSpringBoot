2bcbbd6991ee0854308816e98b12d3cfcca67234
Igor Tudisco <igortudisco@hotmail.com>
2021-06-30 12:14:33 -0300

    CRUD da Loja de Games

---
+++ CRUD_ Back-end_MinhaLojaDeGames/MinhaLojaDeGames/src/main/java/MinhaLojaDeGames/MinhaLojaDeGames/model/Categoria.java
@@ -0,0 +1,19 @@
+package MinhaLojaDeGames.MinhaLojaDeGames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	
+	
+	
+	
+}
---
+++ CRUD_ Back-end_MinhaLojaDeGames/MinhaLojaDeGames/src/test/java/MinhaLojaDeGames/MinhaLojaDeGames/MinhaLojaDeGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package MinhaLojaDeGames.MinhaLojaDeGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaLojaDeGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
